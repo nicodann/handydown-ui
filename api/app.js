c0211aad1b6a8e162c332876eff022e69d3fbd84
Nico <nicodann@gmail.com>
2022-05-06 12:17:25 -0400

    Add sequelize, create database, create user model and user table in db

--- api/app.js
+++ api/app.js
@@ -3,6 +3,33 @@ require('dotenv').config();
 const express = require('express');
 const cookieSession = require('cookie-session');
 const logger = require('morgan');
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres'
+} )
+
+const User = sequelize.define('user', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  location: {
+    type: DataTypes.STRING,
+    allowNull: true
+  }
+})
 
 // const usersRouter = require('./routes/users');
 // const itemsRouter = require('./routes/items');
@@ -22,8 +49,23 @@ app.use(cookieSession({ name: 'session', keys: ['key1'] }));
 
 app.get('/', (req, res) => {
   res.json({"message": 'Welcome to the HandyDown'});
+ 
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`HandyDown API listening on port ${process.env.PORT}`);
-});
+const startApp = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+    await sequelize.sync({ force: true })
+    console.log('All models have been (re) created!')
+    app.listen(process.env.PORT, () => {
+      console.log(`HandyDown API listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+startApp();
+
+

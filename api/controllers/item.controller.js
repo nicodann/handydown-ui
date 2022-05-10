206812035c5d1dc700285a8de32fd91685ff32aa
Nico <nicodann@gmail.com>
2022-05-10 15:49:15 -0400

    Change express to use .json in app.js

--- api/controllers/item.controller.js
+++ api/controllers/item.controller.js
@@ -9,7 +9,7 @@ exports.index = async (req, res) => {
   const condition = name ? { name: { [Op.iLike]: `%${name}%`} } : null;
   try {
     const data = await Item.findAll({ where: condition })
-    res.send(data);
+    res.json(data);
   } catch (err) {
     res.status(499).send({
       message: err.message || "An error occured while retrieving items."
@@ -17,24 +17,10 @@ exports.index = async (req, res) => {
   };
 };
 
-// exports.index = (req, res) => {
-//   const name = req.query.name;
-//   var condition = name ? { name: { [Op.iLike]: `%${name}%`} } : null;
-//   Item.findAll({ where: condition })
-//     .then(data => {
-//       res.send(data);
-//     })
-//     .catch(err => {
-//       res.status(499).send({
-//         message: err.message || "An error occured while retrieving items."
-//       });
-//     });
-// };
-
-
-// Create and save a new Item
-
 exports.create = async (req, res) => {
+
+  console.log(req.header('Content-Type'))
+  console.log(req.body)
 
   const { name, description, image, userId, offered } = req.body
 
@@ -56,13 +42,78 @@ exports.create = async (req, res) => {
   //save
   try {
     data = await Item.create(item);
-    res.send(data);
+    res.json(data);
   } catch (err) {
       res.status(500).send({
         message: err.message || "An error occured while creating the Item."
       });
   };
 };
+
+exports.update = async (req,res) => {
+  const id = req.params.id;
+  try {
+    const num = await Item.update({
+        ...req.body
+      }, {
+        where: { id: id }
+      })
+    if (num == 1) {
+      res.send({
+        message: "Item was update successfully."
+      });
+    } else {
+      res.send({
+        message: `Cannot update Item with id=${id}. Maybe Item was not found or req.body is empty!`
+      });
+    }
+  } catch (err) {
+      res.status(500).send({
+      message: `Error updating Item with id=${id}.`
+      });
+  }
+};
+
+exports.destroy = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const num =  await Item.destroy({
+        where: { id: id }
+      })
+    if (num == 1) {
+      res.send({
+        message: "Item was deleted successfully!"
+      });
+    } else {
+      res.send({
+        message: `Cannot delete item with id=${id}. Maybe item was not found.`
+      });
+    }
+  } catch (err) {
+        res.status(500).send({
+          message: `Could not delete Item with id=${id}.`
+        });
+  };
+};
+
+// exports.index = (req, res) => {
+//   const name = req.query.name;
+//   var condition = name ? { name: { [Op.iLike]: `%${name}%`} } : null;
+//   Item.findAll({ where: condition })
+//     .then(data => {
+//       res.send(data);
+//     })
+//     .catch(err => {
+//       res.status(499).send({
+//         message: err.message || "An error occured while retrieving items."
+//       });
+//     });
+// };
+
+
+// Create and save a new Item
+
+
 // exports.create = (req, res) => {
 //   //validate
 //   if (!req.body.name) {
@@ -98,29 +149,7 @@ exports.create = async (req, res) => {
 
 //Update an Item by the id in the request
 
-exports.update = async (req,res) => {
-  const id = req.params.id;
-  try {
-    const num = await Item.update({
-        ...req.body
-      }, {
-        where: { id: id }
-      })
-    if (num == 1) {
-      res.send({
-        message: "Item was update successfully."
-      });
-    } else {
-      res.send({
-        message: `Cannot update Item with id=${id}. Maybe Item was not found or req.body is empty!`
-      });
-    }
-  } catch (err) {
-      res.status(500).send({
-      message: `Error updating Item with id=${id}.`
-      });
-  }
-};
+
 
 // exports.update = (req,res) => {
 //   const id = req.params.id;
@@ -149,27 +178,7 @@ exports.update = async (req,res) => {
 
 // Delete an Item with the specified id in the request
 
-exports.destroy = async (req, res) => {
-  const id = req.params.id;
-  try {
-    const num =  await Item.destroy({
-        where: { id: id }
-      })
-    if (num == 1) {
-      res.send({
-        message: "Item was deleted successfully!"
-      });
-    } else {
-      res.send({
-        message: `Cannot delete item with id=${id}. Maybe item was not found.`
-      });
-    }
-  } catch (err) {
-        res.status(500).send({
-          message: `Could not delete Item with id=${id}.`
-        });
-  };
-};
+
 // exports.destroy = (req, res) => {
 //   const id = req.params.id;
 //   Item.destroy({

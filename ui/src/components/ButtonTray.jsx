3b5befa286f1991c9ff19c1d7516f2839aae9de0
Nico <nicodann@gmail.com>
2022-05-12 14:38:05 -0400

    Cleanup codel

--- ui/src/components/ButtonTray.jsx
+++ ui/src/components/ButtonTray.jsx
@@ -4,7 +4,7 @@ function ButtonTray() {
   return (
     <div>
         <Button variant="text">register</Button>
-       <Button variant="text">login</Button>
+        <Button variant="text">login</Button>
         <Button variant="disabled">userName</Button>
         <Button variant="text">logout</Button>
         <Button variant="contained">Post Ad</Button>

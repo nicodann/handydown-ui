45d5b26f6641134e7ea9ac73f6fd5738bac5b4ff
Nico <nicodann@gmail.com>
2022-05-12 12:08:38 -0400

    Make Tabs and Searh field in Header

--- ui/src/components/Header.jsx
+++ ui/src/components/Header.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import TabBar from './TabBar';
+import SearchBar from './SearchBar';
 
 function Header() {
   return (
-    <div>Header</div>
+    <>
+      <div className="left">
+        <Typography variant="h1" component="div" gutterBottom>HandyDown</Typography>
+      </div>
+      <div className="right">
+        <Button variant="text">login</Button>
+        <Button variant="text">register</Button>
+        <Button variant="text">logout</Button>
+        <Button variant="contained">Post Ad</Button>
+      </div>
+      <TabBar/>
+      <SearchBar/>
+    </>
   )
 }
 

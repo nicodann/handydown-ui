4ef10fc54932cec3f406187d19d7dc586a0bb47a
Todd Demone <todd.demone.dev@gmail.com>
2022-05-16 15:50:22 -0400

    add 'no results found' text for failed searches; delete Item.jsx

--- ui/src/components/ItemList.jsx
+++ ui/src/components/ItemList.jsx
@@ -31,42 +31,48 @@ function ItemList(props) {
       id={`simple-tabpanel-${tabIndex}`}
     >
       {tabValue === tabIndex && (
+        items && items.length > 0 ? (
         <Box sx={{ flexGrow: 1 }}>
           <Grid container spacing={6}>
-            { items.map((item) => ( 
-              <React.Fragment key={item.id}>
-                <Grid item style={{display: 'flex'}} xs={4} >
-                    <Card style={{height: '100%'}}>
-                      <CardActionArea style={{display: 'flex', flexDirection: 'column', alignItems: 'start', width: '100%', height: '100%'}} > 
-                        <CardHeader
-                          avatar={
-                            <Avatar> 
-                              {item.offered ? "O" : "W"}
-                            </Avatar>
-                          }
-                          title={item.name}
-                          subheader={format(item.createdAt)}
-                        />
-                        <CardMedia
-                          component="img"
-                          height="200"
-                          src={require('../images/baseball-glove.jpg')}
-                          alt={item.description}
-                        />
-                        <CardContent style={{flexGrow: 1}}>
-                          <Typography variant="body2" color="text.secondary">{item.description}</Typography>
-                        </CardContent>
-                        <CardActions style={{width: '100%', paddingLeft: '16px', paddingRight: '16px', display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: 'auto'}}>
-                              <Typography display="block" gutterBottom>{item.user.location}</Typography>
-                              <Typography display="block" gutterBottom>{item.user.username}</Typography>
-                        </CardActions>
-                      </CardActionArea>
-                      </Card>
-                </Grid>
-              </React.Fragment>
-            ))}
+              { items.map((item) => ( 
+                <React.Fragment key={item.id}>
+                  <Grid item style={{display: 'flex'}} xs={4} >
+                      <Card style={{height: '100%'}}>
+                        <CardActionArea style={{display: 'flex', flexDirection: 'column', alignItems: 'start', width: '100%', height: '100%'}} > 
+                          <CardHeader
+                            avatar={
+                              <Avatar> 
+                                {item.offered ? "O" : "W"}
+                              </Avatar>
+                            }
+                            title={item.name}
+                            subheader={format(item.createdAt)}
+                          />
+                          <CardMedia
+                            component="img"
+                            height="200"
+                            src={require('../images/baseball-glove.jpg')}
+                            alt={item.description}
+                          />
+                          <CardContent style={{flexGrow: 1}}>
+                            <Typography variant="body2" color="text.secondary">{item.description}</Typography>
+                          </CardContent>
+                          <CardActions style={{width: '100%', paddingLeft: '16px', paddingRight: '16px', display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: 'auto'}}>
+                                <Typography display="block" gutterBottom>{item.user.location}</Typography>
+                                <Typography display="block" gutterBottom>{item.user.username}</Typography>
+                          </CardActions>
+                        </CardActionArea>
+                        </Card>
+                  </Grid>
+                </React.Fragment>
+              ))}
           </Grid>
         </Box>
+      ) : (
+        <Box display='flex' justifyContent='center'>
+        <Typography variant="body2">No results found!</Typography>
+        </Box>
+      )
       )}
     </div>
   );

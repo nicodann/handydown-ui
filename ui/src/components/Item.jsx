4545a27f0952d11ba9f8da43bf629c0fe88a6c43
Todd Demone <todd.demone.dev@gmail.com>
2022-05-17 06:34:00 -0400

    move individual card to Item.jsx; add image propery to Item props; remove avatar and add 'Offered' and 'Wanted' tags for cards

---
+++ ui/src/components/Item.jsx
@@ -0,0 +1,46 @@
+import { 
+  Avatar,
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Grid,
+  Typography,
+} from '@mui/material';
+import { format } from 'timeago.js';
+
+function Item(props) {
+  const { offered, name, createdAt, image, description, location, username } = props;
+    return (
+    <Grid item style={{display: 'flex'}} xs={4} >
+      <Card style={{height: '100%'}}>
+        <CardActionArea style={{display: 'flex', flexDirection: 'column', alignItems: 'start', width: '100%', height: '100%'}} > 
+          <CardHeader
+            style={{justifyContent: 'space-between', width: '100%'}}
+            title={name}
+            subheader={format(createdAt)}
+            action={ offered ? <Button color="success">Offered</Button> : <Button color="error">Wanted</Button>}
+          />
+          <CardMedia
+            component="img"
+            height="200"
+            src={image}
+            alt={name}
+          />
+          <CardContent style={{flexGrow: 1, width: '100%' }}>
+            <Typography noWrap variant="body2" color="text.secondary">{description}</Typography>
+          </CardContent>
+          <CardActions style={{width: '100%', paddingLeft: '16px', paddingRight: '16px', display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: 'auto'}}>
+            <Typography display="block" gutterBottom>{location}</Typography>
+            <Typography display="block" gutterBottom>{username}</Typography>
+          </CardActions>
+        </CardActionArea>
+        </Card>
+    </Grid>
+    );
+}
+
+export default Item;
--- ui/src/components/ItemList.jsx
+++ ui/src/components/ItemList.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 // import { styled } from '@mui/material/styles';
-import { Box,
-  Avatar,
-  Card,
-  CardActionArea,
-  CardActions,
-  CardContent,
-  CardHeader,
-  CardMedia,
+import { 
+  Box,
   Grid,
   Typography,
 } from '@mui/material';
-import { format } from 'timeago.js';
+import Item from './Item';
 
 // const Item = styled(Paper)(({ theme }) => ({
 //   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -35,36 +29,16 @@ function ItemList(props) {
         <Box sx={{ flexGrow: 1 }}>
           <Grid container spacing={6}>
               { items.map((item) => ( 
-                <React.Fragment key={item.id}>
-                  <Grid item style={{display: 'flex'}} xs={4} >
-                      <Card style={{height: '100%'}}>
-                        <CardActionArea style={{display: 'flex', flexDirection: 'column', alignItems: 'start', width: '100%', height: '100%'}} > 
-                          <CardHeader
-                            avatar={
-                              <Avatar> 
-                                {item.offered ? "O" : "W"}
-                              </Avatar>
-                            }
-                            title={item.name}
-                            subheader={format(item.createdAt)}
-                          />
-                          <CardMedia
-                            component="img"
-                            height="200"
-                            src={require('../images/baseball-glove.jpg')}
-                            alt={item.description}
-                          />
-                          <CardContent style={{flexGrow: 1}}>
-                            <Typography variant="body2" color="text.secondary">{item.description}</Typography>
-                          </CardContent>
-                          <CardActions style={{width: '100%', paddingLeft: '16px', paddingRight: '16px', display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: 'auto'}}>
-                                <Typography display="block" gutterBottom>{item.user.location}</Typography>
-                                <Typography display="block" gutterBottom>{item.user.username}</Typography>
-                          </CardActions>
-                        </CardActionArea>
-                        </Card>
-                  </Grid>
-                </React.Fragment>
+                <Item
+                  key={item.id}
+                  offered={item.offered}
+                  name={item.name}
+                  createdAt={item.createdAt}
+                  image={item.image}
+                  description={item.description}
+                  location={item.user.location}
+                  username={item.user.username}
+                />
               ))}
           </Grid>
         </Box>

a0c39e26c6d95f0c252ff9d3eb0b44722e9e3a41
Todd Demone <todd.demone.dev@gmail.com>
2022-05-19 15:13:55 -0400

    amend styling of SingleItemForm and ReplyForm

--- ui/src/components/Modals/ReplyForm.jsx
+++ ui/src/components/Modals/ReplyForm.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 
-import TextareaAutosize from '@mui/material/TextareaAutosize';
+// import TextareaAutosize from '@mui/material/TextareaAutosize';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 export default function ReplyForm(props) {
 
@@ -11,20 +13,30 @@ export default function ReplyForm(props) {
   const [replyMessage, setReplyMessage] = useState('');
 console.log('replyMessage!!!', replyMessage)
   return (
-    <>
-    <Typography variant="subtitle2">New Message Re: <Typography component="span" variant="caption">Baseball Glove - Offered</Typography></Typography>
-    <TextareaAutosize
-      aria-label="minimum height"
-      minRows={6}
-      placeholder="Minimum 3 rows"
-      // multiline
-      style={{ width: '100%' }}
-      value={replyMessage}
-      onChange={(e) => setReplyMessage(e.target.value) }
-    />
-    <div>
-      <Button onClick={() => replyMessageFunction(replyMessage)} variant="outlined">Reply</Button>
-    </div>
-    </>
+    <Box sx={{mt: 3}}>
+      <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+        <Typography>New Message</Typography>
+        <Typography component="span">Re: Baseball Glove - Offered</Typography>
+      </Box>
+      <TextField
+        type="text"
+        name="description"
+        label="Dear HandyDown User,"
+        multiline
+        rows={5}
+        sx={{ width: '100%'}}
+        // onChange={handleChange}
+      />
+      {/* <TextareaAutosize
+        aria-label="minimum height"
+        minRows={6}
+        placeholder="Minimum 3 rows"
+        // multiline
+        style={{ width: '100%' }}
+        value={replyMessage}
+        onChange={(e) => setReplyMessage(e.target.value) }
+      /> */}
+        <Button onClick={() => replyMessageFunction(replyMessage)} variant="outlined" sx={{mt: 2}}>Reply</Button>
+    </Box>
   );
 }
--- ui/src/components/Modals/SingleItemModal.jsx
+++ ui/src/components/Modals/SingleItemModal.jsx
@@ -5,17 +5,8 @@ import {
   Button,
   Typography,
   Modal,
-  Divider,
-  Card,
-  CardActionArea,
-  CardHeader,
-  // 'onClick' is not defined         no-undef
-  // offered' is not defined         no-undef
-  // 'username' is not defined 
-  CardMedia,
-  CardContent,
-  CardActions,
 } from '@mui/material';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { format } from 'timeago.js';
 import { axios } from 'axios';
@@ -25,7 +16,7 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 600,
+  width: 800,
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
@@ -58,64 +49,36 @@ export default function SingleItemModal(props) {
       aria-describedby="modal-modal-description"
     >
       <Box sx={style}>
-      {/* <Card style={{height: '100%'}}>
-          <CardHeader
-            style={{justifyContent: 'space-between', width: '100%'}}
-            title={name}
-            subheader={format(createdAt)}
-            action={ offered ? <Button component="div" color="success">Offered</Button> : <Button component="div" color="error">Wanted</Button>}
-          />
-          <CardMedia
-            component="img"
-            height="200"
-            src={image}
-            alt={name}
-          />
-          <CardContent style={{flexGrow: 1, width: '100%' }}>
-            <Typography noWrap variant="body2" color="text.secondary">{description}</Typography>
-          </CardContent>
-          <CardActions style={{width: '100%', paddingLeft: '16px', paddingRight: '16px', display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: 'auto'}}>
-            <Typography display="block" gutterBottom>{location}</Typography>
-            <Typography display="block" gutterBottom>{userName}</Typography>
-          </CardActions>
-        </Card> */}
-        {/* <Box
-          sx={{
-            height: 100,
-            maxHeight: { xs: 233, md: 167 },
-            maxWidth: { xs: 350, md: 250 },
-          }}
-          
-        /> */}
-        
-          <Box
-            sx={{display: 'flex', justifyContent: 'space-between'}}
-          >
-            <Typography id="modal-modal-title" variant="h4" component="h2">
-              {name}
-            </Typography>
-            {offered ? <Button component="div" color="success">Offered</Button> : <Button component="div" color="error">Wanted</Button>}
-          </Box>
-          <Typography variant="subheader" display="block" >
+        <Box
+          sx={{display: 'flex', justifyContent: 'space-between'}}
+        >
+          <Typography id="modal-modal-title" variant="h4" component="h2">
+            {name}
+          </Typography>
+          {offered ? <Button component="div" color="success" sx={{mr: -1}}>Offered</Button> : <Button component="div" color="error" sx={{mr: -1}}>Wanted</Button>}
+        </Box>
+        <Box style={{display: 'flex', justifyContent: 'space-between'}}>
+          <Typography>
             {format(createdAt)}
           </Typography>
-        
-        <img src={image} style={{height: '200px', width: '100%', objectFit: 'cover', paddingTop: '16px'}} />
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          <Box style={{display: 'flex', justifyContent: 'space-between'}}>
+            <LocationOnIcon color="primary"/>
+            <Typography display="block">{location}</Typography>
+          </Box>
+        </Box>
+        <img src={image} style={{height: '100%', maxHeight: '350px', width: '100%', objectFit: 'cover', paddingTop: '16px'}} />
+        <Typography id="modal-modal-description" variant="body1" sx={{ mt: 2 }}>
           {description}
         </Typography>
-        <Box style={{width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: 'auto', paddingTop: '24px'}}>
-          <Typography display="block">{location}</Typography>
-          <Typography display="block">{userName}</Typography>
-        </Box>
-        <Divider sx={{mt: 3}} />
+        
+        
         {console.log(
           'loggedInUserID:', loggedInUserID,
           'creatorId:', creatorId
 ,           )}
         {loggedInUserID !== creatorId  &&  <ReplyForm replyMessageFunction={replyMessageFunction} />}
         {tabIndex === 2 && loggedInUserID === creatorId &&
-          <Button variant="contained" startIcon={<DeleteIcon />}>Delete</Button>
+          <Button variant="contained" startIcon={<DeleteIcon />} sx={{mt: 3}}>Delete</Button>
         }
       </Box>
     </Modal>

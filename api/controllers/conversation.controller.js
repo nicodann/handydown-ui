07db58a66922aa2c539a419494163accdd08b1c4
Todd Demone <todd.demone.dev@gmail.com>
2022-05-22 13:49:02 -0400

    cleanup controllers - delete unused image controller; delete unused commented out functions from conversation controller

--- api/controllers/conversation.controller.js
+++ api/controllers/conversation.controller.js
@@ -29,37 +29,3 @@ exports.getByUserId = async (req, res) => {
     res.status(500).send(err); 
   }
 };
-
-// Create a new Conversation and its first message
-// exports.create = async (req, res) => {
-//   const { userId, itemId, body } = req.body;
-//   console.log(itemId)
-//   try {
-    // Check for existing conversation
-    // select * from conversations where itemID = $1, creatorId = $2, receiverId = $3
-    // Conversation.findAll({
-
-    // })
-    // if query returns empty, then create conversation and then create message
-    // if query returns a conversation (value > 0), create message with that conversationId, then update the updatedAt column in the Conversation record
-
-//     const item = await Item.findByPk(itemId);
-    
-//     const conversation = await Conversation.create({
-//       creatorId: userId,
-//       receiverId: item.userId,
-//       itemId: itemId
-//     }); 
-
-//     const message = await Message.create({
-//       body: body,
-//       conversationId: conversation.id,
-//       userId: userId
-//     });
-
-//     return res.json(message);
-//   } catch (err) {
-//     console.log(err);
-//     return res.status(500).send(err);
-//   }
-// };

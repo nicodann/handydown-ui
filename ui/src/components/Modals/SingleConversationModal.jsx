2d23fe50b425f44dfbfd50a352485dd2e3095ac0
Nico <nicodann@gmail.com>
2022-05-23 12:56:58 -0400

    Put logged-in-user's messages on the left always

--- ui/src/components/Modals/SingleConversationModal.jsx
+++ ui/src/components/Modals/SingleConversationModal.jsx
@@ -48,11 +48,12 @@ export default function SingleConversationModal(props) {
 
   const messagesArray = messages.map((message, index) => {
     const bodyStyle = (index === messages.length - 1) ? {...hiddenBodyStyle, ...{ height: 'auto', overflow: 'visible', whiteSpace: 'wrap' } } : hiddenBodyStyle;
+    console.log("loggedInUser:",loggedInUser.id)
     return (
         <Message
           key={message.id}
           user={creator.id === message.userId ? creator.username : receiver.username}
-          aligned={creator.id === message.userId ? "left" : "right"}
+          aligned={loggedInUser.id === message.userId ? "right" : "left"}
           createdAt={message.createdAt}
           body={message.body}
           style={bodyStyle}

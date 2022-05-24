8543049f9a6577572239a848ddd3e9d6db7482ce
Nico <nicodann@gmail.com>
2022-05-24 12:58:39 -0400

    Make Item title fit if they are 2 lines long

--- ui/src/components/Item.jsx
+++ ui/src/components/Item.jsx
@@ -19,7 +19,7 @@ export default function Item(props) {
       <Card style={{height: '100%'}} onClick={onClick}>
         <CardActionArea style={{display: 'flex', flexDirection: 'column', alignItems: 'start', width: '100%', height: '100%'}} > 
           <CardHeader
-            style={{justifyContent: 'space-between', width: '100%', minWidth: '350px'}}
+            style={{justifyContent: 'space-between', width: '100%', minWidth: '350px', minHeight: '120px', alignItems: 'flex-start'}}
             title={name}
             subheader={format(createdAt)}
             action={ offered ? <Button component="div" color="primary">Offered</Button> : <Button component="div" color="warning">Wanted</Button>}

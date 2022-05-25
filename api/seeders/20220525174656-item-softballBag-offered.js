6764644cad09cbaad6ad88bf37eb92fc97f1cb7e
Todd Demone <todd.demone.dev@gmail.com>
2022-05-25 14:05:56 -0400

    fix seed

--- api/seeders/20220525174656-item-softballBag-offered.js
+++ api/seeders/20220525174656-item-softballBag-offered.js
@@ -4,7 +4,7 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('items', [{
       name: "Softball bag",
-      description: "My daughter is off to univeristy and her team will provide her with a new bag. So this one is no longer needed",
+      description: "My daughter is off to university and her team will provide her with a new bag. So this one is no longer needed",
       image: "http://localhost:8080/images/softball-bag.jpg",
       userId: 3,
       offered: true,
